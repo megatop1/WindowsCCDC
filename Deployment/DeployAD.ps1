e86ba8446e163e1a01138d212647e7a1bc13ae57
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-10-01 09:38:00 -0400

    Update DeployAD.ps1

--- Deployment/DeployAD.ps1
+++ Deployment/DeployAD.ps1
@@ -1,6 +1,6 @@
 Install-WindowsFeature -name AD-Domain-Services
 Import-module ADDSDeployment
-Install-ADDSForest -DomainName "ad.ursamentors.com"
+Install-ADDSForest -DomainName "cartel.com"
 $DSRMPassword = password1!
 Install-ADDSForest -DomainName "cartel.com"
 Restart-Computer
