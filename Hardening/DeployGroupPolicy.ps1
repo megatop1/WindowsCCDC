9bb1b7e1e2914dec66bd3a2ec75c0e1d9748e8b1
megatop1 <43348736+megatop1@users.noreply.github.com>
2021-11-17 11:27:46 -0500

    Update DeployGroupPolicy.ps1

--- Hardening/DeployGroupPolicy.ps1
+++ Hardening/DeployGroupPolicy.ps1
@@ -16,17 +16,24 @@ $charCount = ($domainName.ToCharArray() | Where-Object {$_ -eq '.'} | Measure-Ob
 If ($charCount -eq 1) {
 $word1 = $domainArray[0]
 $word2 = $domainArray[1]
+
+#set the value of target for the GPLink
+$target = "dc=$word1,dc=$word2"
 }
 ElseIf ($charCount -eq "2") {
 $word1 = $domainArray[0]
 $word2 = $domainArray[1]
 $word3 = $domainArray[2]
+#set the value of target for the GPLink
+$target = "dc=$word1,dc=$word2,dc=$word3"
 }
 ElseIf ($charCount -eq "3") {
 $word1 = $domainArray[0]
 $word2 = $domainArray[1]
 $word3 = $domainArray[2]
 $word4 = $domainArray[3]
+#set the value of target for the GPLink
+$target = "dc=$word1,dc=$word2,dc=$word3,dc=$word4"
 }
 
 #The below is the name of the domain, rj, com
@@ -36,17 +43,17 @@ $afterDot =
 #Disable CMD for regular users 
 New-GPO -name "DisableCMD" -domain $domainName
 Set-GPRegistryValue -name "DisableCMD" -key "HKCU\Software\Policies\Microsoft\Windows\System" -ValueName "DisableCMD" -type Dword -value 00000002
-New-GPLink -name "DisableCMD" -Target "dc=beforeDot,dc=afterDot" -LinkEnabled Yes 
+New-GPLink -name "DisableCMD" -Target $target" -LinkEnabled Yes 
 
 #Disable access to registry and editing reg keys 
 New-GPO -name "DisableRegAccess" -domain $domainName
 Set-GPRegistryValue -name "DisableRegAccess" -key "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System" -ValueName "DisableRegistryTools" -type Dword -value 00000002
-New-GPLink -name "DisableRegAccess" -Target "dc=beforeDot,dc=afterDot" -LinkEnabled Yes 
+New-GPLink -name "DisableRegAccess" -Target $target -LinkEnabled Yes 
 
 #Disable LLMNR
 New-GPO -name "DisableLLMNR" -domain "domainName"
 Set-GPRegistryValue -name "DisableLLMNR" -key "HKLM\Software\Policies\Microsoft\Windows NT\DNSClient" -ValueName "EnableMulticast" -type Dword -value 0000000
-New-GPLink -name "DisableLLMNR" -Target "dc=beforeDot,dc=afterDot" -LinkEnabled Yes
+New-GPLink -name "DisableLLMNR" -Target $target -LinkEnabled Yes
 
 #Disable RDP (CHECK IF RDP IS SCORED?!?!?!)
 #New-GPO -name "DisableRDP" -domain "domainName"
@@ -56,17 +63,17 @@ New-GPLink -name "DisableLLMNR" -Target "dc=beforeDot,dc=afterDot" -LinkEnabled 
 #Disable forced system restarts after updates
 New-GPO -name "DisableRestarts" -domain "domainName"
 Set-GPRegistryValue -name "DisableRestarts" -key "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -ValueName "NoAutoRebootWithLoggedOnUsers" -type Dword -value 00000001
-New-GPLink -name "DisableRestarts" -Target "dc=beforeDot,dc=afterDot" -LinkEnabled Yes
+New-GPLink -name "DisableRestarts" -Target "$target -LinkEnabled Yes
 
 #Prohibit tasks being started\stopped
 New-GPO -name "DisableTaskCreation" -domain "domainName"
 Set-GPRegistryValue -name "DisableTaskCreation" -key "HKLM\Software\Policies\Microsoft\Windows\Task Scheduler5.0" -ValueName "Execution" -type Dword -value 00000004
-New-GPLink -name "DisableTaskCreation" -Target "dc=beforeDot,dc=afterDot" -LinkEnabled Yes
+New-GPLink -name "DisableTaskCreation" -Target $target -LinkEnabled Yes
 
 #PowerShell Execution Policy to local scripts and signed trusted scripts to execute (Currently not working)
 New-GPO -name "PowershellExecution" -domain "domainName"
 Set-GPRegistryValue -name "PowershellExecution" -key "HKCU\Software\Policies\Microsoft\Windows\PowerShell" -ValueName "EnableScripts" -type Dword -value 00000001
-New-GPLink -name "PowershellExecution" -Target "dc=beforeDot,dc=afterDot" -LinkEnabled Yes
+New-GPLink -name "PowershellExecution" -Target $target -LinkEnabled Yes
 
 
 #Disable SMB1
